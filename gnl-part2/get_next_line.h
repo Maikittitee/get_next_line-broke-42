150bafd88ce792788dc81555fd14ae151c9f55b6
Krittithee Tuncharoen <ktunchar@z2t2c2.42bangkok.com>
2022-11-12 17:44:01 +0700

    iupdate

--- gnl-part2/get_next_line.h
+++ gnl-part2/get_next_line.h
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 21:31:21 by ktunchar          #+#    #+#             */
-/*   Updated: 2022/11/10 23:00:49 by ktunchar         ###   ########.fr       */
+/*   Created: 2022/11/10 23:01:16 by ktunchar          #+#    #+#             */
+/*   Updated: 2022/11/10 23:03:12 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#ifndef	GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-char    *get_next_line(int fd)
-{
-    static char		*buffer;
-    char			*line;
-	char			*next;
+#define BUFFER_SIZE 3
 
-	if (!line)
-		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-
-
-}
-
-int main()
-{
-	int fd;
-
-	fd = open("test",O_RDONLY);
-	if (fd < 0)
-		printf("Cannot Open File\n");
-    
-}
+#endif
