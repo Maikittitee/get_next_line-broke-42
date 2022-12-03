317157b2eb3c0b9fbe6eb45111fb5fcf8a2101db
Krittithee Tuncharoen <ktunchar@z3t5c2.42bangkok.com>
2022-12-03 21:19:49 +0700

    p.earth version

--- gnl-part2/get_next_line.c
+++ gnl-part2/get_next_line.c
@@ -6,11 +6,42 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 21:31:21 by ktunchar          #+#    #+#             */
-/*   Updated: 2022/11/10 23:00:49 by ktunchar         ###   ########.fr       */
+/*   Updated: 2022/12/03 18:11:36 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*read_line(char *buffer, int fd)
+{
+	char *line;
+	char *next;
+	int	n;
+
+
+	line = NULL;
+	if (ft_strlen(buffer))
+	{
+		next = ft_strchr(buffer, '\n');
+		if (next)
+		{
+			*next = '\0';
+			line = ft_strjoin(buffer, "\n");
+			ft_buffercpy(buffer, next + 1);
+			return (line);
+		}
+		line = ft_strjoin(buffer, "");
+		*buffer = 0;
+	}
+	n = read(fd, buffer, BUFFER_SIZE);
+	if (n >= 0)
+		buffer[n] = '\0';
+	if (n > 0 && !line)
+		line = ft_strjoin("","");
+	return (line);
+
+ 
+}
 	
 char    *get_next_line(int fd)
 {
@@ -20,18 +51,24 @@ char    *get_next_line(int fd)
 	int	n;
 
 	line = NULL;
-	if (!next)
+	if (fd < 0)
+		return (NULL);
+	if (!buffer) // there will go to the condition only the first time callin gnl function
 		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	while (!strchr(line, '\n'))
+	next = read_line(buffer, fd);
+	while (next != NULL)
 	{
-		n = read(fd, buffer, BUFFER_SIZE);
-		if (line = NULL)
-			//strdup buffer to line;
-		else
-			//strcat buffer to line;
+		
+		if (line == NULL)
+			line = next;
+		else 
+			line = ft_strjoin(line, next);	
+		if (ft_strchr(line, '\n'))
+			return (line);
+
+		next = read_line(buffer, fd);
 	}
-	//cut only last \n in line
-	//make buffer keep only after \n 
+	free(buffer);
 	return (line);
 
 
@@ -44,5 +81,7 @@ int main()
 	fd = open("test",O_RDONLY);
 	if (fd < 0)
 		printf("Cannot Open File\n");
+	printf("line 1 :%s\n",get_next_line(fd));
+	printf("line 2 :%s\n",get_next_line(fd));
     
 }
--- gnl-part2/get_next_line.h
+++ gnl-part2/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ktunchar <ktunchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 23:01:16 by ktunchar          #+#    #+#             */
-/*   Updated: 2022/11/10 23:03:12 by ktunchar         ###   ########.fr       */
+/*   Updated: 2022/12/03 17:41:21 by ktunchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,17 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 10
+
+char	*ft_strchr(const char *s, int c);
+char    *get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_buffercpy(char *dst, const char *src);
+
+
+
 
 #endif
